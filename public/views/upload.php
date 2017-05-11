ce6cd25421610ca84c1589a2e1b2b9b4ca7d0efb
furkandogan99 <furkanmaster72@gmail.com>
2017-05-11 13:57:20 +0200

    code styling aangepast vanuit comment van eddy.

--- public/views/upload.php
+++ public/views/upload.php
@@ -1,9 +1,8 @@
 <div id="container">
 	<h1>Welcome to CodeIgniter!</h1>
-
 	<div id="body">
-	<div class="file_button_container"><input type="file" /></div>
-		
+		<div class="file_button_container">
+			<input type="file" />
+		</div>
 	</div>
-
 </div>
