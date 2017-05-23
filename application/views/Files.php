30259045de314cc15719bbe6bd02f583839c6650
Fenno Sebrechts <99034086@mydavinci.nl>
2017-05-23 10:47:02 +0200

    no message

--- application/views/Files.php
+++ application/views/Files.php
@@ -1,19 +1,42 @@
-<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-	<section class="content-header">
-		<h1>Files</h1>
-	</section>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>Files</h1>
+    </section>
 
-	<!-- Main content -->
-	<section class="content">
-		<p>Fenno's Page</p>
-<?php foreach($fileNames as $fileName) { ?>
-    		<div class="row">
-				<?= $fileName ?>
-    		</div>
-<?php } ?>
-	</section>
-	<!-- /.content -->
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title">All files uploaded to the database.</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <table id="overviewPeople" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Bestand</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                              	<?php foreach($fileNames as $fileName) { ?>
+						    		<div class="row">
+										<td><?= $fileName ?></td>
+						    		</div>
+								<?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                <!-- /.box-body -->
+                </div>
+            <!-- /.box -->
+            </div>
+        <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </section>
+    <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
