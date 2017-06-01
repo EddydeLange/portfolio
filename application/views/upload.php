3085e97b40227196eab1b3f3b43df9ef7bc9a030
furkandogan99 <furkanmaster72@gmail.com>
2017-06-01 11:14:44 +0200

    no message

--- application/views/upload.php
+++ application/views/upload.php
@@ -1,22 +1,11 @@
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-	<section class="content-header">
-		<h1>Tekst</h1>
-	</section>
 
-	<!-- Main content -->
-	<section class="content">
-		<div class="file_button_container">
-			
-			<form action="csv" method="post" enctype="multipart/form-data">
-		    Select image to upload:
-			    <input type="file" name="fileToUpload" id="fileToUpload">
-			    <input type="submit" value="Upload Image" name="submit">
-			</form>
 
-		</div>
-	</section>
-	<!-- /.content -->
-</div>
-<!-- /.content-wrapper -->
+    
+        <title> PHP MOVE FILE </title>
+    
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="fileName"><br><br>
+            <input type="submit" name="moveFile" value="Upload">
+        </form>
+    
+
