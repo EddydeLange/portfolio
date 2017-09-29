0fb91c0993ef36715f04f8194da81ccfb17754b4
FURKAN\User <frkndgn.99@hotmail.com>
2017-09-29 14:18:40 +0200

    no message

--- application/models/UploadModel.php
+++ application/models/UploadModel.php
@@ -1,34 +1,84 @@
 <?php
-class UploadModel extends CI_Model {
+//class UploadModel extends CI_Model {
 
-    public function uploadFunction()
-    {
-            parent::__construct();
-            // Your own constructor code
-    }
+    //public function uploadFunction()
+    //{
+      //      parent::__construct();
+        //    // Your own constructor code
+    //}
 
-    public function insertFileName($fileData)
+    //public function insertFileName($fileData)
+    //{        
+    	//$this->load->database();
+		//$data = [
+			//'filename' => $fileData['file_name'],
+			//'file_size' => $fileData['file_size'],
+			//'file_date' => date("d-m-Y"),
+			//'file_active' => 1
+		//];
+	//$this->db->insert('imports', $data);
+    //}
+
+    //public function uploadAnswers($fileData)
+    //{
+       // $this->load->database();
+        //$data = [
+          //  'subject_id' => $fileData['subject_id'],
+            //'question_id' => $fileData['question_id'],
+            //'student_id' => $fileData['student_id'],
+            //'answer' => $fileData['answer'],
+            //'date' => date("d-m-Y")
+   //     ];
+     //   $this->db-insert('answers', $data);
+    //}
+//}
+
+class Upload extends CI_Controller {
+
+        public function __construct()
+        {
+                parent::__construct();
+                $this->load->helper(array('form', 'url'));
+        }
+
+        public function index()
+        {
+                $this->load->view('upload_form', array('error' => ' ' ));
+        }
+
+        public function do_upload()
+        {
+                $config['upload_path']          = './uploads/';
+                $config['allowed_types']        = 'csv|gif|jpg|png';
+                $config['max_size']             = 100;
+                $config['max_width']            = 1024;
+                $config['max_height']           = 768;
+
+                $this->load->library('upload', $config);
+
+                if ( ! $this->upload->do_upload('userfile'))
+                {
+                        $error = array('error' => $this->upload->display_errors());
+
+                        $this->load->view('upload_form', $error);
+                }
+                else
+                {
+                        $data = array('upload_data' => $this->upload->data());
+
+                        $this->load->view('upload_success', $data);
+                }
+        }
+        public function insertFileName($fileData)
     {        
-    	$this->load->database();
-		$data = [
-			'filename' => $fileData['file_name'],
-			'file_size' => $fileData['file_size'],
-			'file_date' => date("d-m-Y"),
-			'file_active' => 1
-		];
-	$this->db->insert('imports', $data);
-    }
-
-    public function uploadAnswers($fileData)
-    {
         $this->load->database();
         $data = [
-            'subject_id' => $fileData['subject_id'],
-            'question_id' => $fileData['question_id'],
-            'student_id' => $fileData['student_id'],
-            'answer' => $fileData['answer'],
-            'date' => date("d-m-Y")
+            'filename' => $fileData['file_name'],
+            'file_size' => $fileData['file_size'],
+            'file_date' => date("d-m-Y"),
+            'file_active' => 1
         ];
-        $this->db-insert('answers', $data);
+    $this->db->insert('imports', $data);
     }
 }
+?>
--- application/views/upload.php
+++ application/views/upload.php
@@ -10,17 +10,18 @@
                     <div class="box-header">
                         <h3 class="box-title">Upload a file</h3>
                     </div>
-                    <form role="form" method="POST" action="uploadFile" enctype="multipart/form-data">
-                        <div class="box-body">
-                            <div class="form-group">
-                                <label for="exampleInputFile">File</label>
-                                <input type="file" id="exampleInputFile" name="userfile">
-                            </div>
-                        </div>
-                        <div class="box-footer">
-                            <input type="submit" class="btn btn-primary" name="submit">
-                        </div>
+                    <?php echo $error;?>
+
+                    <?php echo form_open_multipart('upload/do_upload');?>
+
+                    <input type="file" name="userfile" size="20" />
+
+                    <br /><br />
+
+                    <input type="submit" value="upload" />
+
                     </form>
+
                         <div class="col-xs-4">
                     <div class="box">
                 </div>
@@ -28,3 +29,5 @@
         </div>
     </section>
 </div>
+
+
