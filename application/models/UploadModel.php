07d525b47455784985b5907bdcfe343196525750
FURKAN\User <frkndgn.99@hotmail.com>
2017-10-02 14:34:27 +0200

    no message

--- application/models/UploadModel.php
+++ application/models/UploadModel.php
@@ -1,37 +1,37 @@
 <?php
-//class UploadModel extends CI_Model {
+class UploadModel extends CI_Model {
 
-    //public function uploadFunction()
-    //{
-      //      parent::__construct();
-        //    // Your own constructor code
-    //}
+    public function uploadFunction()
+    {
+            parent::__construct();
+            Your own constructor code
+    }
 
-    //public function insertFileName($fileData)
-    //{        
-    	//$this->load->database();
-		//$data = [
-			//'filename' => $fileData['file_name'],
-			//'file_size' => $fileData['file_size'],
-			//'file_date' => date("d-m-Y"),
-			//'file_active' => 1
-		//];
-	//$this->db->insert('imports', $data);
-    //}
+    public function insertFileName($fileData)
+    {        
+    	$this->load->database();
+		$data = [
+			'filename' => $fileData['file_name'],
+			'file_size' => $fileData['file_size'],
+			'file_date' => date("d-m-Y"),
+			'file_active' => 1
+		];
+	$this->db->insert('imports', $data);
+    }
 
-    //public function uploadAnswers($fileData)
-    //{
-       // $this->load->database();
-        //$data = [
-          //  'subject_id' => $fileData['subject_id'],
-            //'question_id' => $fileData['question_id'],
-            //'student_id' => $fileData['student_id'],
-            //'answer' => $fileData['answer'],
-            //'date' => date("d-m-Y")
-   //     ];
-     //   $this->db-insert('answers', $data);
-    //}
-//}
+    public function uploadAnswers($fileData)
+    {
+       $this->load->database();
+        $data = [
+            'subject_id' => $fileData['subject_id'],
+            'question_id' => $fileData['question_id'],
+            'student_id' => $fileData['student_id'],
+            'answer' => $fileData['answer'],
+            'date' => date("d-m-Y")
+        ];
+        $this->db-insert('answers', $data);
+    }
+}
 
 class Upload extends CI_Controller {
 
